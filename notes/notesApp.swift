36067ecec573fddff47b1c5fa02e27bba2246b0f
Marvis Ighedosa <dosamarvis@gmail.com>
2023-10-09 10:07:26 +0100

    Initial Commit

---
+++ notes/notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  notesApp.swift
+//  notes
+//
+//  Created by Marvis Ighedosa on 09/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
